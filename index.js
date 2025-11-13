a8c730b8f6b3d9c57ff4396f642c485a2eda38aa
Nebojsa <savicnebojsa10@gmail.com>
2025-11-13 15:02:23 +0100

    introduce .env file

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
-const mammoth = require("mammoth");
 const morgan = require("morgan");
 const { Worker } = require("worker_threads");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 app.use(morgan("dev"));
@@ -80,4 +82,6 @@ app.post("/convert-text", async (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
